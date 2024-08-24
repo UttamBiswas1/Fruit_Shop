d01c0b7df379b999bd9c58ce5387df9de5fb364f
Uttam Biswas <biswasuttam0911@gmail.com>
2024-08-25 01:03:09 +0530

    Update fruits-model.js

--- backend/models/fruits-model.js
+++ backend/models/fruits-model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/Fruit-Store');
+mongoose.connect('mongodb://0.0.0.0/Fruit-Store');
 const fruitSchema=mongoose.Schema({
  name:String,
  price:Number,
